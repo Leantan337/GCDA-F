5e77f6fdfa1b9f168b0f023de593c143faf83e8a
OMEN <natnael337@gmail.com>
2025-04-16 04:21:11 +0300

    render yaml added disk

--- build.sh
+++ build.sh
@@ -16,3 +16,9 @@ python manage.py createcachetable
 
 # Create Wagtail site entry if needed
 python manage.py shell -c "from wagtail.models import Site; Site.objects.get_or_create(hostname='gcda.onrender.com', port=80, is_default_site=True, root_page_id=1, site_name='GCDA Website')"
+
+# Create superuser if environment variables are set
+#if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+#    echo "Creating superuser..."
+#    python manage.py createsuperuser --noinput
+#fi
