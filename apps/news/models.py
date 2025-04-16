7e45b4807fbd4eac16f164afe76900f7cec389ab
OMEN <natnael337@gmail.com>
2025-04-16 22:17:57 +0300

    Add NewsIndexPage model and templates to fix 500 errors

--- apps/news/models.py
+++ apps/news/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django import forms
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.models import ClusterableModel
@@ -33,6 +34,53 @@ class NewsCategory(models.Model):
         verbose_name_plural = "News Categories"
 
 
+class NewsIndexPage(Page):
+    """Index page for news articles."""
+    template = 'news/news_index.html'
+    
+    intro = models.CharField(max_length=250, blank=True)
+    header_image = models.ForeignKey(
+        get_image_model_string(),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    
+    content_panels = Page.content_panels + [
+        FieldPanel('intro'),
+        FieldPanel('header_image'),
+    ]
+    
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        
+        # Get all live news pages that are children of this index page
+        news_items = NewsPage.objects.live().descendant_of(self).order_by('-date')
+        
+        # Filter by category if the category parameter is in the request
+        category_slug = request.GET.get('category')
+        if category_slug:
+            category = NewsCategory.objects.filter(slug=category_slug).first()
+            if category:
+                news_items = news_items.filter(categories__in=[category])
+                context['current_category'] = category
+        
+        # Pagination
+        paginator = Paginator(news_items, 6)  # Show 6 news items per page
+        page = request.GET.get('page')
+        try:
+            news = paginator.page(page)
+        except PageNotAnInteger:
+            news = paginator.page(1)
+        except EmptyPage:
+            news = paginator.page(paginator.num_pages)
+        
+        context['news_items'] = news
+        context['categories'] = NewsCategory.objects.all()
+        return context
+
+
 class NewsPage(Page):
     """News article page model."""
     template = 'news/news_page.html'
@@ -51,6 +99,9 @@ class NewsPage(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
+    
+    # Parent page / subpage type rules
+    parent_page_types = ['news.NewsIndexPage']
     
     # Search index configuration
     search_fields = Page.search_fields + [
