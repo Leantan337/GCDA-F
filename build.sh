a6afbfd5137cd133b8d3ddcdfd36620f229b59c1
OMEN <natnael337@gmail.com>
2025-04-21 16:58:53 +0300

    s3

--- build.sh
+++ build.sh
@@ -3,14 +3,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# Install dependencies (Render does this automatically, but just in case)
+# Install dependencies
 pip install -r requirements.txt
 
 # Run migrations
 python manage.py migrate --noinput
-
-# Collect static files
-python manage.py collectstatic --noinput
 
 # Create Django superuser if it doesn't exist (fail-safe)
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
--- config/settings/production.py
+++ config/settings/production.py
@@ -5,6 +5,24 @@ import os
 import dj_database_url
 from .base import *  # noqa
 
+# AWS S3 settings
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Media files
+DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaStorage'
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
@@ -12,7 +30,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
 
-# Database - Using dj_database_url for Render compatibility
+# Database - Using dj_database_url for Render compatibilityZ
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
